14f60fd950779efe44ee99ffac5f7a3d0c3425c5
Tarun Singh <tarunrajput639@gmail.com>
2021-01-20 16:22:56 +0530

    Initial commit

---
+++ app/src/main/java/com/mandeep/simplecalculator/MainActivity.java
@@ -0,0 +1,40 @@
+package com.mandeep.simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText firstNumber, secondNumber;
+    TextView output;
+    Button buttonAdd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firstNumber = findViewById(R.id.first_number_input);
+        secondNumber = findViewById(R.id.second_number_input);
+        output = findViewById(R.id.output);
+        buttonAdd = findViewById(R.id.button_add);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNumbers();
+            }
+        });
+    }
+
+    private void addNumbers() {
+        double first = Double.parseDouble(firstNumber.getText().toString());
+        double second = Double.parseDouble(secondNumber.getText().toString());
+        output.setText(String.valueOf(first + second));
+    }
+}
